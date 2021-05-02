09d9a0cfbc36a48b4603ac2338b4d6238a4d8cda
Ofek Shochat <o@shoch.at>
2021-05-02 17:33:22 +0200

    build

--- build.py
+++ build.py
@@ -2,12 +2,12 @@ from sys import argv
 from glob import glob
 import os
 
-outputDir = "./gh-pages/{}"
+outputDir = "./docs/{}"
 styleName = argv[1]
-ARGS =  "c:\\Users\\o\\AppData\\Local\\Pandoc\\pandoc.exe -c ./writings/css/{}.css -s {} -t html -o {} --metadata title=\"{}\""
+ARGS =  "c:\\Users\\o\\AppData\\Local\\Pandoc\\pandoc.exe -c ./writings/css/{}.css -s {} -t html -o {}"
 
 def makecmd(f):
-  return ARGS.format(styleName, f, outputDir.format(f[f.find("\\" if os.name == "nt" else "/"):f.find(".")+1] + "html"), f[:f.find(".")])
+  return ARGS.format(styleName, f, outputDir.format(f[f.find("\\" if os.name == "nt" else "/"):f.find(".")+1] + "html"))
 
 files = glob("writings/*.md")
 files += glob("writings/**/*.md")
